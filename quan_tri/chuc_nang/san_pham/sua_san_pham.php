5c432f6afe204026c284ea672ebb03eac7796f24
TrungNguyenHP <152580377+TrungNguyenHP@users.noreply.github.com>
2024-09-03 13:32:59 +0700

    Update sua_san_pham.php

--- quan_tri/chuc_nang/san_pham/sua_san_pham.php
+++ quan_tri/chuc_nang/san_pham/sua_san_pham.php
@@ -5,7 +5,7 @@
 <?php
 	$id = $_GET['id'];
 	$tv = "SELECT * FROM san_pham WHERE id='$id'";
-	$connection = mysqli_connect("xoaserver.mysql.database.azure.com", "sqladmin", "#Nqthlr123", "ban_hang");
+	$connection = mysqli_connect("coffee-shop.mysql.database.azure.com", "tuan", "Tohru14617", "ban_hang");
 	$result = mysqli_query($connection, $tv);
 	$row = mysqli_fetch_array($result);
 
