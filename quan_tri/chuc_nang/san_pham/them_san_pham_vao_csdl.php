745c25f4b4a4003e0d68bf66e6eee4608c67c6cd
TrungNguyenHP <152580377+TrungNguyenHP@users.noreply.github.com>
2024-09-03 13:34:47 +0700

    Update them_san_pham_vao_csdl.php

--- quan_tri/chuc_nang/san_pham/them_san_pham_vao_csdl.php
+++ quan_tri/chuc_nang/san_pham/them_san_pham_vao_csdl.php
@@ -21,7 +21,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         
         if ($ten != "" && $ten_file_anh != "") {
             // Kết nối đến cơ sở dữ liệu
-            $conn = mysqli_connect("xoaserver.mysql.database.azure.com", "sqladmin", "#Nqthlr123", "ban_hang");
+            $conn = mysqli_connect("coffee-shop.mysql.database.azure.com", "tuan", "Tohru14617", "ban_hang");
             if (!$conn) {
                 die("Lỗi kết nối: " . mysqli_connect_error());
             }
